9e8f48f47dd8f3f36aec0682e39b004975351d24
Alia Zmiievska <103433874+aliazmiievska@users.noreply.github.com>
2026-03-24 17:11:02 +0200

    забрала зайве

--- a.objects/спр/спрСпецификацииНоменклатуры.sql
+++ a.objects/спр/спрСпецификацииНоменклатуры.sql
@@ -1,21 +1,12 @@
 ;WITH 
-
-
--- початкова СпецифікаціяНомекл розрахована на N:N, але фірмою використовується як 1:N, 
--- тому архітектура коду трохи крива
-
-
--- помилки ---------
-spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
+spr_SpecifNomenkl_VyhodIzdelia AS (
     SELECT
         spr_SpecifNomenkl_VyhodIzdelia._Reference207_IDRRef
         ,spr_SpecifNomenkl_VyhodIzdelia._Fld2796RRef AS FK_Номенклатура
         ,spr_SpecifNomenkl_VyhodIzdelia._Fld2798 AS Количество_ВиходногоИзделия
         ,_Reference207_IDRRef AS ID
-
     FROM _Reference207_VT2794 AS spr_SpecifNomenkl_VyhodIzdelia
 )
-
 ,Більше1ВиходнеІздПомилка AS ( -- перевірка 1:N на випадок, якщо фірма почне використовувати як N:N, або станеться баг в базі
     SELECT 
         _Reference207_IDRRef AS ID
@@ -28,23 +19,16 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
     HAVING 
         COUNT(_Reference207_IDRRef) > 1
 )
--- помилки ---------
-
-
--- BASE ------------------------------------------------------------------------------------------
-,spr_SpecifNomenkl AS ( -- ../spr/spr_SpecifNomenkl.sql
+,spr_SpecifNomenkl AS (
     SELECT
         spr_SpecifNomenkl_VyhodIzdelia.FK_Номенклатура AS FK_Номенклатура
-
         ,spr_SpecifNomenkl._IDRRef AS ID
         ,spr_SpecifNomenkl._Code AS Code
         ,spr_SpecifNomenkl._Description AS Назва
         ,spr_SpecifNomenkl._Marked AS ПоміткаВидалення
         ,spr_SpecifNomenkl._Fld2757 AS Активна
         ,spr_SpecifNomenkl_VyhodIzdelia.Количество_ВиходногоИзделия 
-
         ,Більше1ВиходнеІздПомилка.Більше1ВиходнеІздПомилка AS Більше1ВиходнеІздПомилка
-
     FROM 
         _Reference207 AS spr_SpecifNomenkl
         LEFT JOIN 
@@ -55,21 +39,14 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
             Більше1ВиходнеІздПомилка AS Більше1ВиходнеІздПомилка -- перевірка 1:N на випадок N:N
             ON 
                 spr_SpecifNomenkl._IDRRef = Більше1ВиходнеІздПомилка.ID
-
     WHERE
-        spr_SpecifNomenkl._Marked = 0 -- ПоміткаВидалення
+        spr_SpecifNomenkl._Marked = 0
         AND 
-        spr_SpecifNomenkl._Fld2757 = 1 -- Активна
+        spr_SpecifNomenkl._Fld2757 = 1
 )
--- BASE ------------------------------------------------------------------------------------------
-
-
--- табличні ---------------------------------------------------------------
-,spr_SpecifNomenkl_IsxodKomplektuyushie AS ( -- таблична
+,spr_SpecifNomenkl_IsxodKomplektuyushie AS (
     SELECT
         spr_SpecifNomenkl_IsxodKomplektuyushie._Reference207_IDRRef AS FK_spr_SpecifNomenkl
-        -- тут є FK номенклатура, але для 1:N воно не треба
-
         ,spr_SpecifNomenkl_IsxodKomplektuyushie._Fld2772RRef AS ВидНормативаFK
         ,spr_SpecifNomenkl_IsxodKomplektuyushie._Fld2773_RRRef AS НоменклатураFK
         ,spr_SpecifNomenkl_IsxodKomplektuyushie._Fld2775 AS Кількість
@@ -78,39 +55,30 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
         ,spr_SpecifNomenkl_IsxodKomplektuyushie._Fld2781 AS Кратність
         ,spr_SpecifNomenkl_IsxodKomplektuyushie._Fld2782RRef AS ВидПроізводстваFK
         ,spr_SpecifNomenkl_IsxodKomplektuyushie._Fld2783RRef AS СпецифікаціяFK
-        
     FROM 
         _Reference207_VT2770 AS spr_SpecifNomenkl_IsxodKomplektuyushie
 )
-
-,spr_SpecifNomenkl_ParamVypuskaProdukcii AS ( -- таблична
+,spr_SpecifNomenkl_ParamVypuskaProdukcii AS (
     SELECT
         spr_SpecifNomenkl_ParamVypuskaProdukcii._Reference207_IDRRef AS FK_spr_SpecifNomenkl
-
         ,spr_SpecifNomenkl_ParamVypuskaProdukcii._Fld2828RRef AS ВидПараметраFK
         ,spr_SpecifNomenkl_ParamVypuskaProdukcii._Fld2829 AS Значення
     FROM 
         _Reference207_VT2826 AS spr_SpecifNomenkl_ParamVypuskaProdukcii
 )
--- табличні ---------------------------------------------------------------
-
-
--- для пропса -----------------
-,spr_Nomenklatura AS ( -- ../spr/spr_Nomenklatura.sql
+,spr_Nomenklatura AS (
     SELECT
         spr_Nomenklatura._IDRRef AS НоменклатураID
         ,spr_Nomenklatura._Code AS НоменклатураCode
         ,spr_Nomenklatura._Description AS НазваНоменклатури
         ,spr_Nomenklatura._Marked AS ПоміткаВидаленняНоменклатури
         ,spr_Nomenklatura._Folder AS ГрупаНоменклатури
-
         ,spr_Nomenklatura._Fld2306RRef AS FK_ВидНоменклатурьі_Номенклатура
         ,spr_Nomenklatura._Fld23716 AS КількістьВУпаковці_Номенклатура
         ,spr_Nomenklatura._Fld25116RRef AS FK_Проект_КаналЗбуту_Номенклатура
     FROM _Reference149 AS spr_Nomenklatura
 )
-
-,spr_VidyParamProizvods AS ( -- ../spr/spr_VidyParamProizvods.sql
+,spr_VidyParamProizvods AS (
     SELECT
         spr_VidyParamProizvods._IDRRef AS ID_ВидиПарамПроізводства,
         spr_VidyParamProizvods._Code AS Code_ВидиПарамПроізводства,
@@ -119,8 +87,7 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
         spr_VidyParamProizvods._Folder AS ГрупаНоменклатури_ВидиПарамПроізводства
     FROM _Reference23884 AS spr_VidyParamProizvods
 )
-
-,spr_VidyParamVypuskProd AS ( -- ../spr/spr_VidyParamVypuskProd.sql
+,spr_VidyParamVypuskProd AS (
     SELECT
         spr_VidyParamVypuskProd._IDRRef AS ID_ВидиПарамВипускПрод,
         spr_VidyParamVypuskProd._Code AS Code_ВидиПарамВипускПрод,
@@ -129,8 +96,7 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
         spr_VidyParamVypuskProd._Folder AS ГрупаНоменклатури_ВидиПарамВипускПрод
     FROM _Reference56 AS spr_VidyParamVypuskProd
 )
-
-,спр_ЕдиницьіИзмерения AS ( -- ../spr/спр_ЕдиницьіИзмерения.sql
+,спр_ЕдиницьіИзмерения AS (
     SELECT
         спр_ЕдиницьіИзмерения._IDRRef AS ID_ЕдиницьіИзмерения
         ,спр_ЕдиницьіИзмерения._Code AS Code_ЕдиницьіИзмерения
@@ -138,8 +104,7 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
         ,спр_ЕдиницьіИзмерения._Marked AS ПоміткаВидалення_ЕдиницьіИзмерения
     FROM _Reference89 AS спр_ЕдиницьіИзмерения
 )
-
-,спр_СтатьиЗатрат AS ( -- ../spr/спр_СтатьиЗатрат.sql
+,спр_СтатьиЗатрат AS (
     SELECT
         спр_СтатьиЗатрат._IDRRef AS ID_СтатьиЗатрат
         ,спр_СтатьиЗатрат._Code AS Код_СтатьиЗатрат
@@ -147,14 +112,9 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
         ,спр_СтатьиЗатрат._Marked AS ПометкаУдаления_СтатьиЗатрат
     FROM _Reference216 AS спр_СтатьиЗатрат
 )
--- для пропсу -----------------
-
---
--- пропс ----------------------------------------------
 ,propsVyvidChar_SpecifNomenkl AS (
     SELECT
         spr_SpecifNomenkl.FK_Номенклатура AS FK_Номенклатура_Специфікації
-
         ,spr_SpecifNomenkl.Количество_ВиходногоИзделия                  AS Количество_ВиходногоИзделия
         ,spr_SpecifNomenkl.Code                                         AS КодСпецифікації
         ,spr_SpecifNomenkl.Назва                                        AS НазваСпецифікації
@@ -173,16 +133,11 @@ spr_SpecifNomenkl_VyhodIzdelia AS ( -- підфайл
         spr_SpecifNomenkl
         INNER JOIN spr_SpecifNomenkl_ParamVypuskaProdukcii
             ON spr_SpecifNomenkl.ID = spr_SpecifNomenkl_ParamVypuskaProdukcii.FK_spr_SpecifNomenkl
-
-            
         LEFT JOIN spr_VidyParamVypuskProd
             ON spr_SpecifNomenkl_ParamVypuskaProdukcii.ВидПараметраFK = spr_VidyParamVypuskProd.ID_ВидиПарамВипускПрод
-
 UNION ALL
-
 SELECT
         spr_SpecifNomenkl.FK_Номенклатура AS FK_Номенклатура_Специфікації
-
         ,spr_SpecifNomenkl.Количество_ВиходногоИзделия                  AS Количество_ВиходногоИзделия
         ,spr_SpecifNomenkl.Code                                         AS КодСпецифікації
         ,spr_SpecifNomenkl.Назва                                        AS НазваСпецифікації
@@ -201,7 +156,6 @@ SELECT
         spr_SpecifNomenkl
         INNER JOIN spr_SpecifNomenkl_IsxodKomplektuyushie
             ON spr_SpecifNomenkl.ID = spr_SpecifNomenkl_IsxodKomplektuyushie.FK_spr_SpecifNomenkl
-
         LEFT JOIN spr_VidyParamProizvods
             ON spr_SpecifNomenkl_IsxodKomplektuyushie.ВидПроізводстваFK = spr_VidyParamProizvods.ID_ВидиПарамПроізводства
         LEFT JOIN спр_ЕдиницьіИзмерения
@@ -213,9 +167,4 @@ SELECT
         LEFT JOIN spr_SpecifNomenkl AS spr_SpecifNomenkl_Комплектуючі
             ON spr_SpecifNomenkl_IsxodKomplektuyushie.СпецифікаціяFK = spr_SpecifNomenkl_Комплектуючі.ID
 )
--- пропс ----------------------------------------------
-
-
--- відображення, можна змінити --
 SELECT * FROM propsVyvidChar_SpecifNomenkl
--- відображення, можна змінити --
--- a.objects/спр/спрТехнологическиеХарактеристикиНоменклатуры.sql
+++ a.objects/спр/спрТехнологическиеХарактеристикиНоменклатуры.sql
@@ -1,32 +1,22 @@
 ;WITH 
-
------------------------------------------------------------------------------------------------------
-
-spr_TechnolHaraktNomen AS ( -- ../spr/spr_TechnolHaraktNomen.sql
+spr_TechnolHaraktNomen AS (
     SELECT
         spr_TechnolHaraktNomen._Fld24298RRef AS FK_Номенклатура,
-
         spr_TechnolHaraktNomen._IDRRef AS ID,
         spr_TechnolHaraktNomen._Code AS Code,
         spr_TechnolHaraktNomen._Description AS Назва,
         spr_TechnolHaraktNomen._Marked AS ПоміткаВидалення,
         spr_TechnolHaraktNomen._Fld24300 AS Активна
     FROM _Reference24296 AS spr_TechnolHaraktNomen
-
     WHERE 
-        spr_TechnolHaraktNomen._Marked = 0 -- ПоміткаВидалення
+        spr_TechnolHaraktNomen._Marked = 0
         AND 
-        spr_TechnolHaraktNomen._Fld24300 = 1 -- Активна
+        spr_TechnolHaraktNomen._Fld24300 = 1
 ),
---
------------------------------------------------------------------------------------------------------
-
-spr_TechnolHaraktNomen_SpisokHarakt AS ( -- підфайл
+spr_TechnolHaraktNomen_SpisokHarakt AS (
     SELECT
         spr_TechnolHaraktNomen_SpisokHarakt._Reference24296_IDRRef AS FK_Характеристика,
-
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24304RRef AS СвойствоRref,
-
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24305_TYPE AS ТипЗначення,
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24305_S AS ЗначенняСимвольне,
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24305_N AS ЗначенняЧислове,
@@ -34,16 +24,10 @@ spr_TechnolHaraktNomen_SpisokHarakt AS ( -- підфайл
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24305_L AS ЗначенняЛогічне,
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24305_RRRef AS ЗначенняRRRef,
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24305_RTRef AS ЗначенняRTRef,
-
         spr_TechnolHaraktNomen_SpisokHarakt._Fld24306 AS Відхилення
-
-
-
     FROM _Reference24296_VT24302 AS spr_TechnolHaraktNomen_SpisokHarakt
 ),
-
--- for PropsOneColumnChar
-spr_Nomenklatura AS ( -- ../spr/spr_Nomenklatura.sql
+spr_Nomenklatura AS (
     SELECT
         spr_Nomenklatura._IDRRef AS ID,
         spr_Nomenklatura._Code AS Code,
@@ -52,9 +36,7 @@ spr_Nomenklatura AS ( -- ../spr/spr_Nomenklatura.sql
         spr_Nomenklatura._Folder AS Група
     FROM _Reference149 AS spr_Nomenklatura
 ),
-
--- for PropsOneColumnChar
-spr_ZnachSvoistvOb AS ( -- ../spr/spr_ZnachSvoistvOb.sql
+spr_ZnachSvoistvOb AS (
     SELECT
         spr_ZnachSvoistvOb._IDRRef AS ID,
         spr_ZnachSvoistvOb._Code AS Code,
@@ -64,40 +46,30 @@ spr_ZnachSvoistvOb AS ( -- ../spr/spr_ZnachSvoistvOb.sql
     FROM
         _Reference93 AS spr_ZnachSvoistvOb
 ),
-
--- for PropsOneColumnChar
-planVidHar_TechSvoistvaOb AS ( -- ../spr/planVidHar_TechSvoistvaOb.sql
+planVidHar_TechSvoistvaOb AS (
     SELECT
         planVidHar_TechSvoistvaOb._IDRRef AS ID,
         planVidHar_TechSvoistvaOb._Code AS Code,
         planVidHar_TechSvoistvaOb._Description AS NameN,
         planVidHar_TechSvoistvaOb._Marked AS ПоміткаВидалення,
-
         planVidHar_TechSvoistvaOb._Fld24417RRef AS ТипВластивості,
         planVidHar_TechSvoistvaOb._Type AS Тип,
         planVidHar_TechSvoistvaOb._Fld24398RRef AS ВаріантРозрахунку,
-
         planVidHar_TechSvoistvaOb._Fld24399_TYPE AS ТипДанихДляРозрахунку,
         planVidHar_TechSvoistvaOb._Fld24399_RRRef AS РозрахунокПосилання,
         planVidHar_TechSvoistvaOb._Fld24399_N AS РозрахунокЧисло
     FROM _Chrc24297 AS planVidHar_TechSvoistvaOb
 ),
-
--- всі свойства в колонці, тип чар
-PropsOneColumnChar AS ( -- пропс
-
+PropsOneColumnChar AS (
     SELECT
         spr_TechnolHaraktNomen.FK_Номенклатура AS FK_Номенклатура_Характеристики,
-
         spr_TechnolHaraktNomen.Code AS КодХарактеристики,
         spr_TechnolHaraktNomen.Назва AS НазваХарактеристики,
         planVidHar_TechSvoistvaOb.NameN AS НазваСвойства,
         spr_TechnolHaraktNomen_SpisokHarakt.Відхилення AS Відхилення,
-
         CASE -- все конвертується, бо кейс приймає тільки один тип даних (в півоті будуть нормальні типи)
             WHEN spr_TechnolHaraktNomen_SpisokHarakt.ТипЗначення = 0x1 
                 THEN CONVERT(NVARCHAR(100), spr_TechnolHaraktNomen_SpisokHarakt.ЗначенняДата, 120)
-
             WHEN spr_TechnolHaraktNomen_SpisokHarakt.ТипЗначення = 0x2 
                 THEN 
                     CASE 
@@ -105,23 +77,16 @@ PropsOneColumnChar AS ( -- пропс
                             THEN N'Так'
                         ELSE N'Ні'
                     END
-
             WHEN spr_TechnolHaraktNomen_SpisokHarakt.ТипЗначення = 0x3 
                 THEN CAST(spr_TechnolHaraktNomen_SpisokHarakt.ЗначенняЧислове AS NVARCHAR(100))
-
             WHEN spr_TechnolHaraktNomen_SpisokHarakt.ТипЗначення = 0x5                -- знаходила вручну
                 THEN CAST(spr_TechnolHaraktNomen_SpisokHarakt.ЗначенняСимвольне AS NVARCHAR(100))
-
             WHEN spr_TechnolHaraktNomen_SpisokHarakt.ЗначенняRTRef = 0x00000095
                 THEN spr_Nomenklatura.НазваНоменклатури
-
             WHEN spr_TechnolHaraktNomen_SpisokHarakt.ЗначенняRTRef = 0x0000005D
                 THEN spr_ZnachSvoistvOb.НазваСвойства
-
             ELSE NULL
-                
         END AS Значення
-
     FROM spr_TechnolHaraktNomen
         LEFT JOIN spr_TechnolHaraktNomen_SpisokHarakt
             ON spr_TechnolHaraktNomen.ID = spr_TechnolHaraktNomen_SpisokHarakt.FK_Характеристика
@@ -134,7 +99,5 @@ PropsOneColumnChar AS ( -- пропс
         LEFT JOIN planVidHar_TechSvoistvaOb
             ON spr_TechnolHaraktNomen_SpisokHarakt.СвойствоRref = planVidHar_TechSvoistvaOb.ID
 )
-
--- відображення, можна змінити
 SELECT * FROM PropsOneColumnChar
 ORDER BY КодХарактеристики
