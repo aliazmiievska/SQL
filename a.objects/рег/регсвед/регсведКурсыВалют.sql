e91d00da536737b7761066c8caa77ccb2f2d8394
Alia Zmiievska <103433874+aliazmiievska@users.noreply.github.com>
2026-03-24 11:15:40 +0200

    dfdfdff

--- a.objects/рег/регсвед/регсведКурсыВалют.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM _InfoRg17986
-LEFT JOIN _Reference35 AS док_ЗаказПокупателя
-    ON _InfoRg17986._Fld17987RRef = док_ЗаказПокупателя._IDRRef
-ORDER BY _Period DESC
--- a.objects/спр/спрПодразделения.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM _Reference171
