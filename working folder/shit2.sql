1e91472656bad2eb60ab149d16fd297860801f01
Alia Zmiievska <103433874+aliazmiievska@users.noreply.github.com>
2026-04-03 15:23:54 +0300

    шось зробила

--- working folder/shit2.sql
+++ working folder/shit2.sql
@@ -1,29 +1,11 @@
-
-
--- А
--- Б
--- В
--- Г
--- Д
--- Е
--- Ж
--- З
--- И
--- К
--- Л
--- М
--- Н
--- О
--- П
--- Р
--- С
--- Т
--- У
--- Ф
--- Х
--- Ц
--- Ч
--- Ш
--- Щ
--- Ю
--- Я
+SELECT * FROM _Reference23089
+WHERE _Reference23089._Description LIKE N'%ЗП %'
+AND _Reference23089._Description <> 'ЗП HR'
+AND _Reference23089._Description <> 'ЗП АКОМ'
+AND _Reference23089._Description <> 'ЗП Бухгалтерія'
+AND _Reference23089._Description <> 'ЗП Копійочка'
+AND _Reference23089._Description <> 'ЗП Мерчендайзінг'
+AND _Reference23089._Description <> 'ЗП Охорона праці'
+AND _Reference23089._Description <> 'ЗП Рушники'
+AND _Reference23089._Description <> 'ЗП Склад'
+ORDER BY _Description
