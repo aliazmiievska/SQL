e8666e299a625f465f0a1ac7eb74bdb69720b797
Alia Zmiievska <103433874+aliazmiievska@users.noreply.github.com>
2026-04-10 11:12:10 +0300

    бумс

--- working folder/shit1.sql
+++ working folder/shit1.sql
@@ -1,26 +1,5 @@
-SELECT
-    DATEFROMPARTS(
-        own__Рік__КалендарРікМісяцьДо40.own__Рік__КалендарРікМісяцьДо40,
-        own__Місяць__КалендарРікМісяцьДо40.own__Місяць__КалендарРікМісяцьДо40,
-        1
-    ) own__ДатаРікМісяць__КалендарРікМісяцьДо40
-FROM (
-    SELECT 
-        2025 + systemTable.number own__Рік__КалендарРікМісяцьДо40
-    FROM 
-        master..spt_values systemTable
-    WHERE 
-        systemTable.type = 'P'
-        AND 
-        systemTable.number <= 15
-) own__Рік__КалендарРікМісяцьДо40
-CROSS JOIN (
-    SELECT 
-        systemTable.number + 1 own__Місяць__КалендарРікМісяцьДо40
-    FROM 
-        master..spt_values systemTable
-    WHERE 
-        systemTable.type = 'P'
-        AND 
-        systemTable.number < 12
-) own__Місяць__КалендарРікМісяцьДо40
+SELECT name
+FROM sys.tables
+WHERE name LIKE '_Document%'
+AND NOT name LIKE '%_VT%'
+ORDER BY name
